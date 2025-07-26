1ac70cd3a3dd132b99ab87639a87fd71f0a196d6
thealoneshadow <amayauta12@gmail.com>
2025-07-26 23:04:53 +0530

    Auth Service chanegs

--- microservices/ai-tasks/src/index.ts
+++ microservices/ai-tasks/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.send('Auth Service is up!');
+  res.send('AI Service is up!');
 });
 
 const PORT = process.env.PORT || 3001;
