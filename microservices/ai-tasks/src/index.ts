31cc4809fdef8b6d83a44f1729585f7f1770ba6d
thealoneshadow <amayauta12@gmail.com>
2025-07-26 17:26:11 +0530

    microservices folder strcuture

---
+++ microservices/ai-tasks/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.send('Auth Service is up!');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Auth Service running on port ${PORT}`);
+});
